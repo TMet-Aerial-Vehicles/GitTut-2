146e4d7a207616ce650ba5e294d6738293833f52
CGCCollin <ccollington@torontomu.ca>
2023-09-14 20:11:25 -0400

    Initial commit

---
+++ code.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plot
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+y = np.array([1,4,9,16,25])
+
+plot.plot(x,y)
+#what could possibly be wrong here? (HINT HINT) ↓↓↓
+plot.shoe()
